ac5c816926b01a00d297ad1ab6c2fdcde5e25b8a
mounikadevarapu11-rgb <mounikadevarapu09@gmail.com>
2026-02-25 22:15:17 +0530

    🚀 Day 29 - index.html - 10:14:28 pm

---
+++ Day-29/script.js
@@ -0,0 +1,7 @@
+// Day 29 Logic Practice
+console.log('Lab Session 29 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
